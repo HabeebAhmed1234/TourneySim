60abdf26fd4c5bd0346a5fdc698d4ba741385977
Habeeb Ahmed <1habeebahmed1@gmail.com>
2019-09-04 17:53:07 -0400

    sync

--- src/com/tourney/Constants.java
+++ src/com/tourney/Constants.java
@@ -11,4 +11,7 @@ public class Constants {
     public static final int FEES_PER_PLAYER_DOLLARS = 5;
     public static final int NUM_TEAMS_PER_TOURNAMENT = 100;
     public static final long GAME_LENGTH_MINS = TimeUnit.HOURS.toMinutes(2);
+
+    // Team generation constants
+    public static final long TEAM_INTERVAL_AVERAGE_START_TIME_IN_DAY_MINS =
 }
---
+++ src/com/tourney/TeamsGenerator.java
@@ -0,0 +1,11 @@
+package com.tourney;
+
+/**
+ * Creates teams and their corresponding time preferences
+ */
+public class TeamsGenerator {
+
+    public static TeamsPool generateStartingTeamsPool() {
+
+    }
+}
